0c78f99bf564aa57214d263ebc88bda7b3d349c1
Davu <41762352+ldavu@users.noreply.github.com>
2021-11-29 14:49:17 -0300

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,13 @@
+def mostrarMenu():
+    # mostra o menu
+    print("Selecione um objeto:")
+    print("0 - Porta")
+    print("1 - Janela")
+    print("2 - Mochila")
+    print("3 - Vaso")
+    print("4 - Gaveta")
+    print("5 - Quadro")
+    print("6 - Baú")
+    print("7 - Tapete")
+    print("8 - Espelho")
+    print("9 - Estante")
